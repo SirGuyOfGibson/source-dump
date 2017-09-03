4e84dc31d52412a9d58d5f0c54f5514c0eac5137
Nicholas <nickama777@gmail.com>
2017-09-03 19:09:26 -0400

    Select and Create and List

--- console.py
+++ console.py
@@ -1,14 +1,33 @@
 from dumpster import Dumpster
 import os
 
-i = input('\r>')
+running = True
+selected = ''
 
-if i == 'list':
-    cwd = os.getcwd()
-    lcd = os.listdir()
-    dump = ''
-    for file in lcd:
-        if '.dmp' in file:
-            dump+= ' '+file
+while running:
+    #cwd = os.getcwd()
 
-    print(dump)
+    i = input('\r%s>'%(selected))
+
+    if i == 'exit':
+        running = False
+
+    if i[0:6] == 'create':
+        name = i[7:]
+        Dumpster(name).write_to_dump()
+
+    if i == 'list':
+        if selected is 'none':    #list currrent working directory
+            dirs = ''
+            lcd = os.listdir()
+            for file in lcd:
+                if '.dmp' in file:
+                    dirs+= ' '+file.strip('.dmp')
+            print(dirs)
+
+        else:                    #list selected dump
+            #.......................................
+
+    if i[0:6] == 'select':
+        name = i[7:]
+        selected = name
